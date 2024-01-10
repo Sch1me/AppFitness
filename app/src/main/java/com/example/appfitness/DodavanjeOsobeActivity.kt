3a69984246caaaa8a958e6e2cf43927a22eabf63
SimeZorki <simebusiness7@gmail.com>
2024-01-10 22:35:54 +0100

    Firebase radi, RV ne radi, ocajnicki pokusaj na VjezbeAdapterr

--- app/src/main/java/com/example/appfitness/DodavanjeOsobeActivity.kt
+++ app/src/main/java/com/example/appfitness/DodavanjeOsobeActivity.kt
@@ -12,6 +12,7 @@ import com.example.appfitness.ObjektniModel.VjezbeModel
 import com.example.appfitness.databinding.ActivityDodavanjeOsobeBinding
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
+
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
--- app/src/main/java/com/example/appfitness/VjezbeAdapter.kt
+++ app/src/main/java/com/example/appfitness/VjezbeAdapter.kt
@@ -3,11 +3,22 @@ package com.example.appfitness
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.appfitness.ObjektniModel.OsobeModel
 import com.example.appfitness.ObjektniModel.VjezbeModel
 import com.example.appfitness.databinding.OsobeItemBinding
 import com.example.appfitness.databinding.VjezbeItemBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import java.lang.Exception
+
+private val dataBase: DatabaseReference =
+    FirebaseDatabase.getInstance("https://appfitness-68156-default-rtdb.europe-west1.firebasedatabase.app/").getReference("Vjezbe")
 
 class VjezbeAdapter(
     private val vjezbeList: ArrayList<VjezbeModel>,
@@ -30,10 +41,24 @@ class VjezbeAdapter(
     class ViewHolder(private var itemBinding: VjezbeItemBinding):
         RecyclerView.ViewHolder(itemBinding.root){
         fun bindItem(vjezbeModel: VjezbeModel, th: Context){
-            itemBinding.textViewImeVjezbe.text = vjezbeModel.imeVjezbe
-            itemBinding.textViewBrojSerija.text = vjezbeModel.brojSerija
-            itemBinding.textViewBrojPonavljanja.text = vjezbeModel.brojPonavljanja
-            itemBinding.textViewVrstaVjezbe.text = vjezbeModel.vrstaVjezbe
+
+            dataBase.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    try {
+                        itemBinding.textViewImeVjezbe.text = snapshot.child("imeVjezbe").getValue().toString()
+                        itemBinding.textViewBrojSerija.text = snapshot.child("brojSerija").getValue().toString()
+                        itemBinding.textViewBrojPonavljanja.text = snapshot.child("brojPonavljanja").getValue().toString()
+                        itemBinding.textViewVrstaVjezbe.text = snapshot.child("vrstaVjezbe").getValue().toString()
+
+                    }catch (_: Exception){}
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(th,"Failed with database",Toast.LENGTH_SHORT).show()
+                }
+            })
+
         }
     }
 
